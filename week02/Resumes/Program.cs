88126404d3fc4af7a728d23b0ed74e6cb0cd00db
almabarraza <barrazaraygozalic@gmail.com>
2025-01-14 20:56:50 -0700

    Finished Resumes practice

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+       
+        Job Job1 = new Job();
+        Job1._jobTitle = "Software Engineer";
+        Job1._company = "Microsoft";
+        Job1._startYear = "2000";
+        Job1._endYear = "2010";
+        
+    
+
+        Job Job2 = new Job();
+        Job2._jobTitle = "Software Developer";
+        Job2._company = "Apple";
+        Job2._startYear = "2011";
+        Job2._endYear = "2020";
+
+     
+
+        Resume Resume1 = new Resume();
+        Resume1._name = "Alma Barraza";
+        Resume1._jobs.Add(Job1);
+        Resume1._jobs.Add(Job2);
+        
+        Resume1.Display();
+      
+
+
     }
 }
