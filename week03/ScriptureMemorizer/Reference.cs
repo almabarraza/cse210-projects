99916bf75fcd37164e7d51b734887564f7f08a61
almabarraza <barrazaraygozalic@gmail.com>
2025-01-23 21:47:14 -0700

    Finished Design

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endverse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+
+    }
+
+    public Reference(string book, int chapter, int startverse, int endverse)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return"";
+        
+    }
+}
